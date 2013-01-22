883c7a76a40aeb927690cb2eab8f26bbcb70a36a
Raymond T. Hightower <rth@wisdomgroup.com>
2013-01-21 18:31:40 -0600

    added navigationController

--- MMNavController/MMNavController/AppDelegate.m
+++ MMNavController/MMNavController/AppDelegate.m
@@ -23,8 +23,13 @@
 {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
+
     self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController" bundle:nil] autorelease];
-    self.window.rootViewController = self.viewController;
+
+    navigationController = [[UINavigationController alloc] initWithRootViewController:self.viewController];    
+    
+    self.window.rootViewController = navigationController;
+    
     [self.window makeKeyAndVisible];
     return YES;
 }
